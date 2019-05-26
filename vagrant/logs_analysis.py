a9e42969d2fa394f967183cd9055e0bc61f34a83
joyce yu <yumengnan@gmail.com>
2019-05-26 16:20:51 -0700

    revised with code review

--- vagrant/logs_analysis.py
+++ vagrant/logs_analysis.py
@@ -1,118 +1,140 @@
+#!/usr/bin/env python
+
 import psycopg2
-from datetime import datetime
 
 
-# 1. What are the most popular three articles of all time? Which articles have been accessed the most?
-# Present this information as a sorted list with the most popular article at the top.
 def get_mp_articles(n=3, dbname="news"):
-  db = psycopg2.connect(database=dbname)
-  c = db.cursor()
-  c.execute ("""
-    select 
-      articles.title, count(log.path) as num 
-    from articles 
-      join log on ('/article/' || articles.slug) = log.path  
-    group by articles.slug, articles.title
-    order by num desc limit 3;
-  """)
-  mp_articles = c.fetchall()  
-  db.close()
-  result = []
+    """
+    Return rankings of articles based on view counts.
 
-  for row in mp_articles:
-    result.append( '"{}" -- {} views'.format(row[0], str(row[1])) )
+    Args:
+    n (optional int) - sets the number of articles to return. Defaults to 3.
 
-  return result 
+    dbname(optional string) - sets the name of the database to connect to.
+                              Default to "news"
+
+    Returns:
+    A list of strings. Each string contains:
+      - the title of the article.
+      - the number of views for the article
+
+    The list is sorted by number of views in descending order.
+    """
+
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    c.execute("""
+      select
+        title, views
+      from articles
+      inner join (
+        select path, count(path) as views
+        from log
+        group by log.path
+      ) as log
+      on ('/article/' || articles.slug) = log.path
+      order by views desc
+      limit 3;
+    """)
+    mp_articles = c.fetchall()
+    db.close()
+    result = []
+
+    for row in mp_articles:
+        result.append('"{}" -- {} views'.format(row[0], str(row[1])))
+
+    return result
 
 
-# 2. Who are the most popular article authors of all time? 
-# That is, when you sum up all of the articles each author has written, which authors get the most page views?
-# Present this as a sorted list with the most popular author at the top.
 def get_mp_authors(dbname="news"):
-  db = psycopg2.connect(database=dbname)
-  c = db.cursor()
-  c.execute ("""
-    select 
-      authors.name, count(log.path) as num 
-    from authors 
-      left join articles on authors.id = articles.author
-      join log on ('/article/' || articles.slug) = log.path
-    group by authors.name
-    order by num desc;
-  """)
-  mp_authors = c.fetchall()  
-  db.close()
-  result = []
+    """
+    Return rankings of authors based on view counts.
 
-  for row in mp_authors:
-    result.append('{} -- {} views'.format(row[0], str(row[1])) )
+    Args:
+    dbname(optional string) - sets the name of the database to connect to.
+                              Default to "news"
 
-  return result
- 
- 
+    Returns:
+    A list of strings. Each string contains:
+      - the name of the author.
+      - the number of views for the author
 
-# 3. On which days did more than 1% of requests lead to errors? 
-# The log table includes a column status that indicates the HTTP status code that the news site sent to 
-# the user's browser. (Refer to this lesson for more information about the idea of HTTP status codes.)
+    The list is sorted by number of views in descending order.
+    """
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    c.execute("""
+      select
+        name, views
+      from authors
+        left join articles on authors.id = articles.author
+        inner join (
+          select path, count(path) as views
+          from log
+          group by log.path
+        ) as log on ('/article/' || articles.slug) = log.path
+      order by views desc;
+    """)
+    mp_authors = c.fetchall()
+    db.close()
+    result = []
+
+    for row in mp_authors:
+        result.append('{} -- {} views'.format(row[0], str(row[1])))
+
+    return result
+
+
 def get_error_rate(dbname="news"):
-  db = psycopg2.connect(database=dbname)
-  c = db.cursor()
-  c.execute ("""
-    create view count_by_date as 
-      select 
-        time::date as date,
-        count(*) as log_count
-      from log 
-      group by date;
-    
-    create view error_by_date as
-      select 
-        time::date as date,
-        count(*) as error_count 
-      from log 
-      where status like '4%' or status like '5%' 
-      group by date;
-  
-    create view error_rate_by_date as
-      select 
-        c.date, 
-        1.0 * error_count/log_count as error_rate 
-      from count_by_date as c join error_by_date as e on c.date = e.date;
+    """
+    Return dates on which the error rate of accessing articles is bigger
+    than 1%
 
-    select 
-      date,
-      error_rate
-    from error_rate_by_date
-    where error_rate > 0.01;
-  """)
-  error_rate = c.fetchall()  
-  db.close()
-  result = []
+    Args:
+    dbname(optional string) - sets the name of the database to connect to.
+                              Default to "news"
 
-  for row in error_rate:
-    ds = datetime.strftime(row[0], "%b %d, %Y")
-    result.append('{} -- {}% errors'.format(ds, str(round(row[1]*100,1)) ))
-    
-  return result
+    Returns:
+    A list of strings. Each string contains:
+      - the date
+      - the error rate
+
+    The list is sorted by number of views in descending order.
+    """
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    c.execute("""
+      select
+        date,
+        error_rate
+      from error_rate_by_date
+      where error_rate > 0.01;
+    """)
+    error_rate = c.fetchall()
+    db.close()
+    result = []
+
+    for row in error_rate:
+        result.append('{:%B %d, %Y} -- {:.1%} errors'.format(row[0], row[1]))
+
+    return result
 
 
 if __name__ == '__main__':
-  mp_articles = get_mp_articles()
-  print "1. What are the most popular three articles of all time?\n"
-  for line in mp_articles:
-    print line
-  
-  print "\n"
-  
-  mp_authors = get_mp_authors()
-  print "2. Who are the most popular article authors of all time?\n"
-  for line in mp_authors:
-    print line
+    mp_articles = get_mp_articles()
+    print "1. What are the most popular three articles of all time?\n"
+    for line in mp_articles:
+        print line
+    print "\n"
 
-  error_rate = get_error_rate()
-  print "\n"
+    mp_authors = get_mp_authors()
+    print "2. Who are the most popular article authors of all time?\n"
+    for line in mp_authors:
+        print line
 
-  print "3. On which days did more than 1% of requests lead to errors? \n"
-  for line in error_rate:
-    print line
- 
+    error_rate = get_error_rate()
+    print "\n"
+
+    print "3. On which days did more than 1% of requests lead to errors? \n"
+    for line in error_rate:
+        print line
